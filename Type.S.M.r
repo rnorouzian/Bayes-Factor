9a5e1926b6dc4ad7e8db43d2c85f0c2bc846ae6e
Reza Norouzian <rnorouzian@gmail.com>
2017-07-30 12:41:20 -0500

    Update Type.S.M.r

--- Type.S.M.r
+++ Type.S.M.r
@@ -34,8 +34,8 @@ type.s.m = function(n1 = 20, n2 = NA, d = .1, obs.d = .6){
                   ylab = NA, font = 2, font.lab = 2, yaxt = "n", bty = "n",
                   cex.axis = 1, cex.lab = 1, yaxs = "i", ty = "n")
   
-   x = seq(min.d, CI[1], l = 1e4)   ;  y = dt(x/d.SE, df, ncp)/d.SE
-  xx =  seq(max.d, CI[2], l = 1e4)  ; yy = dt(xx/d.SE, df, ncp)/d.SE 
+     x = seq(min.d, CI[1], l = 1e4)   ;  y = dt(x/d.SE, df, ncp)/d.SE
+    xx =  seq(max.d, CI[2], l = 1e4)  ; yy = dt(xx/d.SE, df, ncp)/d.SE 
   
   polygon(c(min.d, x, CI[1]), c(y[1], y, rev(y[1])), col = 2, border = NA)  
   polygon(c(max.d, xx, CI[2]), c(yy[1], yy, rev(yy[1])), col = 2, border = NA) 
@@ -49,14 +49,16 @@ type.s.m = function(n1 = 20, n2 = NA, d = .1, obs.d = .6){
   segments(c(CI[1], CI[2]), 0, c(CI[1], CI[2]), 2, lty = 2, col = 2, xpd = NA)
   
   type.s.area = pt(CI[1]/d.SE, df, ncp)
-  power = type.s.area + pt(CI[2]/d.SE, df, ncp, lower.tail = FALSE)
-  type.s = type.s / power
-  p.value = 2*pt(abs(obs.d)/d.SE, df, lower.tail = FALSE)
-  random.d = rt(n = 1e6, df, ncp)*d.SE
-  sig = abs(random.d) > CI[2]
+        power = type.s.area + pt(CI[2]/d.SE, df, ncp, lower.tail = FALSE)
+       type.s = type.s / power
+      p.value = 2*pt(abs(obs.d)/d.SE, df, lower.tail = FALSE)
+     random.d = rt(n = 1e6, df, ncp)*d.SE
+          sig = abs(random.d) > CI[2]
   exaggration = mean(abs(random.d)[sig]) / d
   
   list(exaggration.ratio = exaggration, type.s = type.s, power = power, Crit.d = CI[2], p.value = p.value)
 }
 
-type.s.m(n1 = 20, d = .1, obs.d = .6) 
+# Example used in the Supplementry Document:
+
+ type.s.m(n1 = 20, d = .1, obs.d = .6) 
