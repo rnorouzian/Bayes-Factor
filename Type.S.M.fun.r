a28cbd36c4387c03dc99c3463d634f280e918752
Reza Norouzian <rnorouzian@gmail.com>
2017-07-31 12:30:58 -0500

    Update Type.S.M.fun.r

--- Type.S.M.fun.r
+++ Type.S.M.fun.r
@@ -1,23 +1,22 @@
-
-##############################################
-## Draw functions type.s, exagration vs. power
-##############################################
+########################################################
+## Draw functions for type.s, exagration each vs. power
+########################################################
 
 type.s.mfun = function(n1, n2 = NA, d.min = 0, d.max = 1.4){
   
   type.s.m2 = function(n1, n2 = NA, d){
     
-    N = ifelse(is.na(n2), n1, (n1 * n2)/(n1 + n2))
-    df = ifelse(is.na(n2), n1 - 1, (n1 + n2) - 2)
-    d.SE = 1/sqrt(N) ; ncp = d*sqrt(N)
-    CI = qt(c(.025, .975), df)*d.SE
+          N = ifelse(is.na(n2), n1, (n1 * n2)/(n1 + n2))
+         df = ifelse(is.na(n2), n1 - 1, (n1 + n2) - 2)
+       d.SE = 1/sqrt(N) ; ncp = d*sqrt(N)
+         CI = qt(c(.025, .975), df)*d.SE
     
-    type.s.area = pt(ifelse(d > 0, CI[1]/d.SE, CI[2]/d.SE), df, ncp, lower.tail = ifelse(d > 0, TRUE, FALSE))
-    power = type.s.area + pt(ifelse(d > 0, CI[2]/d.SE, CI[1]/d.SE), df, ncp, lower.tail = ifelse(d > 0, FALSE, TRUE))
-    type.s = type.s.area / power
-    random.d = rt(n = 1e4, df, ncp)*d.SE
-    sig = if(d > 0) abs(random.d) > CI[2] else -abs(random.d) < CI[1]
-    exaggration = if(d > 0) mean(abs(random.d)[sig])/ d else mean(-abs(random.d)[sig])/ d
+type.s.area = pt(ifelse(d > 0, CI[1]/d.SE, CI[2]/d.SE), df, ncp, lower.tail = ifelse(d > 0, TRUE, FALSE))
+      power = type.s.area + pt(ifelse(d > 0, CI[2]/d.SE, CI[1]/d.SE), df, ncp, lower.tail = ifelse(d > 0, FALSE, TRUE))
+     type.s = type.s.area / power
+   random.d = rt(n = 1e4, df, ncp)*d.SE
+        sig = if(d > 0) abs(random.d) > CI[2] else -abs(random.d) < CI[1]
+exaggration = if(d > 0) mean(abs(random.d)[sig])/ d else mean(-abs(random.d)[sig])/ d
     
     list(exaggration = exaggration, type.s = type.s, power = power)
   }
@@ -27,17 +26,17 @@ type.s.mfun = function(n1, n2 = NA, d.min = 0, d.max = 1.4){
   
   par(mfrow = c(2, 1), mgp = c(2, .5, 0), mar = c(4, 4, 3, 2), las = 1)  
   
-  d_range = seq(d.min, d.max, by = 1e-3)
-  n = length(d_range)
-  power = numeric(n)
-  type.s = numeric(n)
-  exaggration = numeric(n)
+    d_range = seq(d.min, d.max, by = 1e-3)
+          n = length(d_range)
+      power = numeric(n)
+     type.s = numeric(n)
+exaggration = numeric(n)
   
-  for (i in 1:n){
-    a = type.s.m2(d = d_range[i], n1 = n1, n2 = n2)
-    power[i] = a$power
-    type.s[i] = a$type.s
-    exaggration[i] = a$exaggration
+for (i in 1:n){
+             a = type.s.m2(d = d_range[i], n1 = n1, n2 = n2)
+      power[i] = a$power
+     type.s[i] = a$type.s
+exaggration[i] = a$exaggration
   }
   
   plot(power, type.s, ty = "l", xaxt = "n", lwd = 2, font.lab = 2)
