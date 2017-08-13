497c327cca1360908da0e350b429bf84f4ba642f
Reza Norouzian <rnorouzian@gmail.com>
2017-08-12 23:11:54 -0500

    Update Type.S.M.fun.r

--- Type.S.M.fun.r
+++ Type.S.M.fun.r
@@ -1,15 +1,16 @@
-########################################################
-## Draw functions for type.s, exagration each vs. power
-########################################################
 
-type.s.mfun = function(n1, n2 = NA, d.min = 0, d.max = 1.4){
+###############################################
+## Draws functions type.s, exagration vs. power
+###############################################
+
+type.s.mfun = function(n1, n2 = NA, d.min = 0, d.max = 1.4, alpha = .05){
   
-   type.s.m = function(n1, n2 = NA, d){
+  type.s.m = function(n1, n2, d, alpha){
     
           N = ifelse(is.na(n2), n1, (n1 * n2)/(n1 + n2))
          df = ifelse(is.na(n2), n1 - 1, (n1 + n2) - 2)
        d.SE = 1/sqrt(N) ; ncp = d*sqrt(N)
-         CI = qt(c(.025, .975), df)*d.SE
+         CI = qt(c(alpha/2, 1-(alpha/2)), df)*d.SE
     
 type.s.area = pt(ifelse(d > 0, CI[1]/d.SE, CI[2]/d.SE), df, ncp, lower.tail = ifelse(d > 0, TRUE, FALSE))
       power = type.s.area + pt(ifelse(d > 0, CI[2]/d.SE, CI[1]/d.SE), df, ncp, lower.tail = ifelse(d > 0, FALSE, TRUE))
@@ -26,25 +27,27 @@ exaggration = if(d > 0) mean(abs(random.d)[sig])/ d else mean(-abs(random.d)[sig
   
   par(mfrow = c(2, 1), mgp = c(2, .5, 0), mar = c(4, 4, 3, 2), las = 1)  
   
-       d_range = seq(d.min, d.max, by = 1e-3)
+       d_range = seq(d.min, d.max, by = .005)
              n = length(d_range)
          power = numeric(n)
         type.s = numeric(n)
    exaggration = numeric(n)
   
-for (i in 1:n){
-             a = type.s.m(d = d_range[i], n1 = n1, n2 = n2)
+  for (i in 1:n){
+             a = type.s.m(d = d_range[i], n1 = n1, n2 = n2, alpha = alpha)
       power[i] = a$power
      type.s[i] = a$type.s
 exaggration[i] = a$exaggration
   }
   
-  plot(power, type.s, ty = "l", xaxt = "n", lwd = 2, font.lab = 2)
-  axis(1, at = c(.05, seq(.2, 1, by = .2)))
-  plot(power, exaggration, ty = "l", ylim = c(1, 10), yaxt = "n", lwd = 2, font.lab = 2)
-  abline(h = 1, col = "grey")
+  plot(power, type.s, ty = "l", xaxt = "n", lwd = 2, font.lab = 2, col = 2)
+  axis(1, at = c(alpha, seq(.2, 1, by = .2)))
+  abline(v = alpha, col = 8)
+  plot(power, exaggration, ty = "l", ylim = c(1, 10), xaxt = "n", yaxt = "n", lwd = 2, font.lab = 2, col = 4)
+  axis(1, at = c(alpha, seq(.2, 1, by = .2)))
   axis(2, at = seq(1, 10, by = 2))
+  abline(h = 1, v = alpha, col = 8)
   
 }
-
-type.s.mfun(n1 = 15, n2 = 15)
+# Example of use:
+type.s.mfun(n1 = 10, alpha = .05)
